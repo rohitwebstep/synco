9a0e35790883186914b35e94e2eb504ee994747a
rohitwebstep <rohitwebstep@gmail.com>
2025-07-15 16:33:13 +0530

    update - 4

--- src/routes/admin/notification/customNotificationRoutes.js
+++ src/routes/admin/notification/customNotificationRoutes.js
@@ -4,11 +4,15 @@ const authMiddleware = require("../../../middleware/admin/authenticate");
 
 const {
     createCustomNotification,
+    markNotificationAsRead,
     getAllCustomNotifications
 } = require("../../../controllers/admin/notification/customNotificationController");
 
 // Create a new notification
 router.post("/", authMiddleware, createCustomNotification);
+
+// Mark a notification as read (expects notificationId in body or query)
+router.patch("/read", authMiddleware, markNotificationAsRead);
 
 // Get all notifications
 router.get("/", authMiddleware, getAllCustomNotifications);
